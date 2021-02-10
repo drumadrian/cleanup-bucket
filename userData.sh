d239fba02a883864a3c72f6e21f746fc9fde6ba9
Adrian Drummond <adrian.drummond@gmail.com>
2021-02-10 01:03:40 -0800

    refactoring user data script and removing environment variable fiel

--- userData.sh
+++ userData.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#################################################################
+## Create directory
+#################################################################
 yum update -y
-mkdir /home/ec2-user/cleanupBucketDirectory
-cd /home/ec2-user/cleanupBucketDirectory
+DIR=/home/ec2-user/cleanupBucketDirectory
+REPO=https://raw.githubusercontent.com/drumadrian/cleanup-bucket/main
+mkdir $DIR
+cd $DIR
 
 
 #################################################################
 ## Download files from GitHub
 #################################################################
-curl https://raw.githubusercontent.com/drumadrian/cleanup-bucket/main/requirements.txt -o requirements.txt
-curl https://raw.githubusercontent.com/drumadrian/cleanup-bucket/main/cleanupBucket.py -o cleanupBucket.py
+curl $REPO/requirements.txt -o requirements.txt
+curl $REPO/requirements.txt -o cleanupBucket.py
 
 
 #################################################################
@@ -21,26 +26,17 @@ export es_index_name="python_logger_cleanupbucket"
 export environment="Dev"
 export es_host="elasticsearch-domain"
 
-echo "" > /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "logging_level=$logging_level" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "bucket_name=$bucket_name" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "delete_bucket=$delete_bucket" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "es_index_name=$es_index_name" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "environment=$environment" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-echo "es_host=$es_host" >> /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-
 
 #################################################################
 ## install prerequisites
 #################################################################
-sudo yum install python3 -y
-source /home/ec2-user/cleanupBucketDirectory/cleanupBucket.env
-sudo pip3 install -r /home/ec2-user/cleanupBucketDirectory/requirements.txt
+yum install python3 -y
+pip3 install -r requirements.txt
 
 #################################################################
 ## Start the bucket cleanup script
 # Recommended: 
-#       python3 /home/ec2-user/cleanupBucketDirectory/cleanupBucket.py >> cleanupBucket_output.txt
+#       python3 $DIR/cleanupBucket.py >> $DIR/cleanupBucket_output.txt
 #################################################################
-python3 /home/ec2-user/cleanupBucketDirectory/cleanupBucket.py
+python3 cleanupBucket.py
 
